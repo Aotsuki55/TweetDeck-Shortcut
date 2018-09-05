a7561019f65c9b252e1c578b376560f1fd7e327c
aotsuki <39661418+Aotsuki55@users.noreply.github.com>
2018-09-05 18:00:46 +0900

    例外処理

--- TweetDeck-Shortcut.user.js
+++ TweetDeck-Shortcut.user.js
@@ -21,122 +21,128 @@ var elm;
 var elemFound;
 var elemFound2;
 var num = 0;
+var error;
 
 document.addEventListener('keydown', function (e) {
-  var pressed = String.fromCharCode(e.which).toLowerCase();
-  pressed = (e.ctrlKey ? 'C-' : '') + (e.altKey ? 'A-' : '') + (e.shiftKey ? 'S-' : '') + pressed;
-  if (pressed != shortcutKey4){
-    num=0;
-  }
-  if (INPUTS.indexOf(e.target.tagName) == -1 && (pressed == shortcutKey1 || pressed == shortcutKey2 || pressed == shortcutKey3 || pressed == shortcutKey4 )) {
-    e.preventDefault();
-    if (!disableKey) {
-      disableKey = true;
-      pXPathStr = getXpath(elm);
-      var u=0;
-      switch(pressed){
-        case shortcutKey1:
-        case shortcutKey2:
-          for(var t=0;t<14;++t){
-            while(pXPathStr[u]!='/'){
-              ++u;
-              if(u>300) {
-                disableKey = false;
-                return;
-              }
-            }
-            ++u;
-          }
-          --u;
-          pXPathStr=pXPathStr.substr(0,u);
-          var likeXpath = pXPathStr + "/div/div/footer/ul/li[3]/a";
-          var dlXpath = pXPathStr + "/div/div/footer/ul/li[4]/a";
-
-          elemFound = document.evaluate(likeXpath, document, null, 0, null).iterateNext();
-          if(elemFound){
-            elemFound.click();
-          }
-
-          if(pressed != shortcutKey2){
-            elemFound = document.evaluate(dlXpath, document, null, 0, null).iterateNext();
-            if(elemFound){
-              elemFound.click();
-            }
-          }
-          break;
-        case shortcutKey3:
-          for(var s=0;s<10;++s){
-            while(pXPathStr[u]!='/'){
-              ++u;
-              if(u>300) {
-                disableKey = false;
-                return;
-              }
-            }
-            ++u;
-          }
-          --u;
-          pXPathStr=pXPathStr.substr(0,u);
-          var retXpath = pXPathStr + "/header/a";
-          elemFound = document.evaluate(retXpath, document, null, 0, null).iterateNext();
-          if(elemFound){
-            elemFound.click();
-          }
-          break;
-        case shortcutKey4:
-          for(var v=0;v<10;++v){
-            while(pXPathStr[u]!='/'){
-              ++u;
-              if(u>300) {
-                disableKey = false;
-                return;
-              }
-            }
-            ++u;
-          }
-          --u;
-          pXPathStr=pXPathStr.substr(0,u);
-          var clearXpath;
-          var clearXpath2;
-          if(num==0){
-            num=1;
-            clearXpath = pXPathStr + "/header/div/div[1]";
-          }else if(num==1) {
-            num=2;
-            clearXpath = pXPathStr + "/header/div/div[2]/a[3]";
-            elemFound = document.evaluate(clearXpath, document, null, 0, null).iterateNext();
-            if(!elemFound){
-              clearXpath = clearXpath.slice( 0, -3 );
-            }
-          }else if(num==2) {
-            num=0;
-            clearXpath = pXPathStr + "/div[1]/div[1]/div[1]/form/fieldset[2]/div[3]/button";
-            clearXpath2 = pXPathStr + "/header/div/div[2]/a[3]";
-            elemFound2 = document.evaluate(clearXpath2, document, null, 0, null).iterateNext();
-            if(!elemFound2){
-              clearXpath2 = clearXpath2.slice( 0, -3 );
-              elemFound2 = document.evaluate(clearXpath2, document, null, 0, null).iterateNext();
-            }
-          }
-          elemFound = document.evaluate(clearXpath, document, null, 0, null).iterateNext();
-          if(elemFound){
-            elemFound.click();
-            if(num==0){
-              if(elemFound2){
-                elemFound2.click();
-              }
-            }
-          }
-          break;
-      }
-      disableKey = false;
+  try{
+    var pressed = String.fromCharCode(e.which).toLowerCase();
+    pressed = (e.ctrlKey ? 'C-' : '') + (e.altKey ? 'A-' : '') + (e.shiftKey ? 'S-' : '') + pressed;
+    if (pressed != shortcutKey4){
+      num=0;
     }
+    if (INPUTS.indexOf(e.target.tagName) == -1 && (pressed == shortcutKey1 || pressed == shortcutKey2 || pressed == shortcutKey3 || pressed == shortcutKey4 )) {
+        e.preventDefault();
+        if (!disableKey) {
+            disableKey = true;
+            pXPathStr = getXpath(elm);
+            var u=0;
+            switch(pressed){
+              case shortcutKey1:
+              case shortcutKey2:
+                for(var t=0;t<14;++t){
+                    while(pXPathStr[u]!='/'){
+                        ++u;
+                        if(u>300) {
+                            disableKey = false;
+                            return;
+                        }
+                    }
+                    ++u;
+                }
+                --u;
+                pXPathStr=pXPathStr.substr(0,u);
+                var likeXpath = pXPathStr + "/div/div/footer/ul/li[3]/a";
+                var dlXpath = pXPathStr + "/div/div/footer/ul/li[4]/a";
+
+                elemFound = document.evaluate(likeXpath, document, null, 0, null).iterateNext();
+                if(elemFound){
+                      elemFound.click();
+                }
+
+                if(pressed != shortcutKey2){
+                  elemFound = document.evaluate(dlXpath, document, null, 0, null).iterateNext();
+                  if(elemFound){
+                        elemFound.click();
+                  }
+                }
+                break;
+              case shortcutKey3:
+                for(var s=0;s<10;++s){
+                    while(pXPathStr[u]!='/'){
+                        ++u;
+                        if(u>300) {
+                            disableKey = false;
+                            return;
+                        }
+                    }
+                    ++u;
+                }
+                --u;
+                pXPathStr=pXPathStr.substr(0,u);
+                var retXpath = pXPathStr + "/header/a";
+                elemFound = document.evaluate(retXpath, document, null, 0, null).iterateNext();
+                if(elemFound){
+                    elemFound.click();
+                }
+                break;
+              case shortcutKey4:
+                for(var v=0;v<10;++v){
+                    while(pXPathStr[u]!='/'){
+                        ++u;
+                        if(u>300) {
+                            disableKey = false;
+                            return;
+                        }
+                    }
+                    ++u;
+                }
+                --u;
+                pXPathStr=pXPathStr.substr(0,u);
+                var clearXpath;
+                var clearXpath2;
+                if(num==0){
+                  num=1;
+                  clearXpath = pXPathStr + "/header/div/div[1]";
+                }else if(num==1) {
+                  num=2;
+                  clearXpath = pXPathStr + "/header/div/div[2]/a[3]";
+                  elemFound = document.evaluate(clearXpath, document, null, 0, null).iterateNext();
+                  if(!elemFound){
+                    clearXpath = clearXpath.slice( 0, -3 );
+                  }
+                }else if(num==2) {
+                  num=0;
+                  clearXpath = pXPathStr + "/div[1]/div[1]/div[1]/form/fieldset[2]/div[3]/button";
+                  clearXpath2 = pXPathStr + "/header/div/div[2]/a[3]";
+                  elemFound2 = document.evaluate(clearXpath2, document, null, 0, null).iterateNext();
+                  if(!elemFound2){
+                    clearXpath2 = clearXpath.slice( 0, -3 );
+                    elemFound2 = document.evaluate(clearXpath2, document, null, 0, null).iterateNext();
+                  }
+                }
+                elemFound = document.evaluate(clearXpath, document, null, 0, null).iterateNext();
+                if(elemFound){
+                  elemFound.click();
+                  if(num==0){
+                      if(elemFound2){
+                          elemFound2.click();
+                      }
+                  }
+                }
+                break;
+            }
+            disableKey = false;
+        }
+    }
+  }
+  catch(error){
+    disableKey = false;
   }
 }, false);
 
 
 document.onmouseover = function (e){
-  elm = document.elementFromPoint(e.clientX, e.clientY);
+    elm = document.elementFromPoint(e.clientX, e.clientY);
 };
 
 
